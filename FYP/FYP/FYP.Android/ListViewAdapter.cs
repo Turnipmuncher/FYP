bdcabdf6f1f7fec0d4d53e3fd6e6410c85f33371
Turnipmuncher <q5233000@live.tees.ac.uk>
2018-03-18 17:56:54 +0000

    Added firebase.
    Firebase has been added, app is able to add name and email address to firebase

---
+++ FYP/FYP/FYP.Android/ListViewAdapter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FYP.Droid.Model;
+
+namespace FYP.Droid
+{
+    public class ListViewAdapter : BaseAdapter
+    {
+        Activity activity;
+        List<Account> lstAccounts;
+        LayoutInflater inflater;
+
+        public ListViewAdapter(Activity activity, List<Account> lstAccounts)
+        {
+            this.activity = activity;
+            this.lstAccounts = lstAccounts;
+        }
+        public override int Count
+        {
+            get { return lstAccounts.Count; }
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            inflater = (LayoutInflater)activity.BaseContext.GetSystemService(Context.LayoutInflaterService);
+            View itemView = inflater.Inflate(Resource.Layout.list_Item, null);
+            var txtuser = itemView.FindViewById<TextView>(Resource.Id.list_name);
+            var txtemail = itemView.FindViewById<TextView>(Resource.Id.list_email);
+            if (lstAccounts.Count > 0)
+            {
+                txtuser.Text = lstAccounts[position].name;
+                txtemail.Text = lstAccounts[position].email;
+            }
+            return itemView;
+        }
+    }
+}
